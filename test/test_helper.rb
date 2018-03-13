f459cb32f701d08564072df16ebada3ab7a39545
Alejok28 <alejo_k28@hotmail.com>
2018-03-12 20:44:40 -0500

    Adds travis yml

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,21 +7,5 @@ class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
-
   # Add more helper methods to be used by all tests here...
 end
-
-# 
-# class ActionDispatch::SystemTestCase
-#   include Capybara::DSL
-#   include Capybara::Minitest::Assertions
-#
-#   # Reset sessions and driver between tests
-#   # Use super wherever this method is redefined in your individual test classes
-#   def teardown
-#     Capybara.reset_sessions!
-#     # Capybara.use_default_driver
-#     Capybara.default_driver = :selenium # :selenium_chrome and :selenium_chrome_headless are also registered
-#
-#   end
-# end
